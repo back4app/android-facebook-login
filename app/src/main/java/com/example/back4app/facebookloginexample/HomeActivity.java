8cbbf8531a1c9e96dd0577fa776d760f26d20212
Thayna Baldao <thayna.baldao@gmail.com>
2018-01-16 09:50:04 -0200

    adding files

---
+++ app/src/main/java/com/example/back4app/facebookloginexample/HomeActivity.java
@@ -0,0 +1,74 @@
+package com.example.back4app.facebookloginexample;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import com.facebook.login.LoginManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(HomeActivity.this);
+        setContentView(R.layout.activity_home);
+
+
+        final Button logout_button = findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog dlg = new ProgressDialog(HomeActivity.this);
+                dlg.setTitle("Please, wait a moment.");
+                dlg.setMessage("Logging out...");
+                dlg.show();
+
+                // logging out of Facebook
+                LoginManager.getInstance().logOut();
+
+                // logging out of Parse
+                ParseUser.logOut();
+
+                alertDisplayer("So, you're going...", "Ok...Bye-bye then");
+
+            }
+        });
+
+
+    }
+
+    private void alertDisplayer(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/back4app/facebookloginexample/LoginActivity.java
@@ -0,0 +1,133 @@
+package com.example.back4app.facebookloginexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.lang.String;
+import java.util.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(this);
+        ParseFacebookUtils.initialize(this);
+        setContentView(R.layout.activity_login);
+
+        final Button login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog dlg = new ProgressDialog(LoginActivity.this);
+                dlg.setTitle("Please, wait a moment.");
+                dlg.setMessage("Logging in...");
+                dlg.show();
+
+                Collection<String> permissions = Arrays.asList("public_profile", "email");
+
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, permissions, new LogInCallback() {
+
+                    @Override
+                    public void done(ParseUser user, ParseException err) {
+                        if (err != null) {
+                            Log.e("err", "err", err);
+                        }
+                        if (user == null) {
+                            Toast.makeText(LoginActivity.this, "The user cancelled the Facebook login.", Toast.LENGTH_LONG).show();
+                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                            dlg.dismiss();
+                        } else if (user.isNew()) {
+                            dlg.dismiss();
+                            Toast.makeText(LoginActivity.this, "User signed up and logged in through Facebook.", Toast.LENGTH_LONG).show();
+                            Log.d("MyApp", "User signed up and logged in through Facebook!");
+                            getUserDetailFromFB();
+                        } else {
+                            dlg.dismiss();
+                            Toast.makeText(LoginActivity.this, "User logged in through Facebook.", Toast.LENGTH_LONG).show();
+                            Log.d("MyApp", "User logged in through Facebook!");
+                            alertDisplayer("Oh, you!","Welcome back!");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void getUserDetailFromFB(){
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),new GraphRequest.GraphJSONObjectCallback(){
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                ParseUser user = ParseUser.getCurrentUser();
+                try{
+                    user.setUsername(object.getString("name"));
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+                try{
+                    user.setEmail(object.getString("email"));
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+                user.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        alertDisplayer("First Time Login!", "Welcome!");
+                    }
+                });
+            }
+
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","name,email");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void alertDisplayer(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+}
